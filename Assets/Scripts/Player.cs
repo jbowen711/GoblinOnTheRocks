fa1d066af306112b48c15b8e4a0685567556de6c
Jonathan Bowen <jonathanbowen7@gmail.com>
2024-03-20 21:51:11 -0400

    Cinemachine Addition

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
     public Rigidbody rb;
     public float moveSpeed;
     public float boostPower;
@@ -21,18 +22,22 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        //Get Player Input Horizontal and Vertical
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
+        //Rigid Body movement calculations
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
 
+        //Dash using boostPower in movementDirection
         if(Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(moveDirection.normalized * boostPower, ForceMode.Impulse);
 
         }
 
+        //Orientation of facing diection
         if (moveDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
