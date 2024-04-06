b1443e0fd25eab750976b3a942d967c88d843ade
madzwo <madzwo@iu.edu>
2024-04-06 16:15:03 -0400

    Menu Complete

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,6 +10,13 @@ public class Menu : MonoBehaviour
     public Canvas howToPlayCanvas;
     public Canvas creditsCanvas;
 
+    public GameObject tree1;
+    public GameObject tree2;
+    public GameObject tree3;
+    public GameObject tree4;
+    public GameObject tree5;
+
+
     public void StartGame()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -20,6 +27,12 @@ public class Menu : MonoBehaviour
     {
         menuCanvas.gameObject.SetActive(false);
         howToPlayCanvas.gameObject.SetActive(true);
+        tree1.gameObject.SetActive(false);
+        tree2.gameObject.SetActive(false);
+        tree3.gameObject.SetActive(false);
+        tree4.gameObject.SetActive(false);
+        tree5.gameObject.SetActive(false);
+
     }
 
     public void Credits()
@@ -33,6 +46,11 @@ public class Menu : MonoBehaviour
         menuCanvas.gameObject.SetActive(true);
         howToPlayCanvas.gameObject.SetActive(false);
         creditsCanvas.gameObject.SetActive(false);
+        tree1.gameObject.SetActive(true);
+        tree2.gameObject.SetActive(true);
+        tree3.gameObject.SetActive(true);
+        tree4.gameObject.SetActive(true);
+        tree5.gameObject.SetActive(true);
     }
 
     public void Quit()
